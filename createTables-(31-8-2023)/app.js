5b92604f9f5e4cc9ad4d50f9b4cd76b300ebe789
Dhinakaran616 <dhinakaran.ds616@gmail.com>
2023-08-31 11:08:19 +0530

    Multiplication tables using for loop

---
+++ createTables-(31-8-2023)/app.js
@@ -0,0 +1,9 @@
+// Create multiplication Tables using for loop
+
+for(let i = 1; i<=20; i++){
+    console.log(`multiplication table  ${i}`);
+
+    for(let j=1; j<=10; j++){
+        console.log(`${j} * ${i} = ${i * j}`)
+}
+}
